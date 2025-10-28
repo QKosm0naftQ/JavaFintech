73b06e3e4c3c14fb2c0811e4eaf0f370fc3122e7
Kostiantyn <kostya190807@gmail.com>
2025-10-28 19:56:07 +0100

    Перший коміт , перша пара код

---
+++ Projects/1.Begin/README.md
@@ -0,0 +1,29 @@
+### https://reactnative.dev/docs/set-up-your-environment?os=linux == VScode
+# Встановлення компоментів
+```
+sudo dnf install nodejs java-17-openjdk
+```
+# Яка версія java і nodejs
+```
+> java -version
+openjdk version "21.0.8" 2025-07-15
+OpenJDK Runtime Environment (Red_Hat-21.0.8.0.9-1) (build 21.0.8+9)
+OpenJDK 64-Bit Server VM (Red_Hat-21.0.8.0.9-1) (build 21.0.8+9, mixed mode, sharing)
+> node -v
+v22.20.0
+```
+# Скачати Extension Pack for Java з vscode
+
+# Скачати окремо maven для java
+### Створення проекту
+```
+mvn archetype:generate -DgroupId=com.mycompany.app -DartifactId=my-app -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.5 -DinteractiveMode=false
+```
+## Запуск та очищення  (тут різні параметри , можуть бути окремо)
+```
+mvn compile exec:java -Dexec.mainClass="com.mycompany.app.App" 
+mvn clean compile exec:java -Dexec.mainClass="com.mycompany.app.App"
+mvn clean
+mvn
+mvn exec:java -Dexec.mainClass="com.mycompany.app.App"
+```
---
+++ Projects/1.Begin/my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,43 @@
+package com.mycompany.app;
+import java.util.Random;
+import java.util.Scanner;
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+//        One();
+//        boolean t = true;
+        //short, double, float, char, long, String
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Вкажіть розмір масиву");
+        int n = Integer.parseInt(scanner.nextLine());
+        int [] array = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            array[i] = GetRandom(1, 100);
+        }
+
+        for(var item : array) {
+            System.out.print(item + "\t");
+        }
+//        for (int i = 0; i < n; i++) {
+//            System.out.print(array[i] + "\t");
+//        }
+        System.out.println();
+    }
+
+    public static int GetRandom(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min + 1) + min;
+    }
+
+    public static void One() {
+        int age;
+        System.out.println("Вкажіть Ваш вік:");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        age = Integer.parseInt(str);
+        System.out.println("Вам зараз " + age);
+    }
+}
